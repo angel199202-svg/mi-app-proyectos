a2c1fe1f23e576531a230aff62dabea37365ab3b
angel <angel.1992.02@gmail.com>
2026-06-19 11:11:34 -0400

    fix(admin): add missing Link import in invitados page

--- app/admin/invitados/page.tsx
+++ app/admin/invitados/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import Link from 'next/link'
 import { supabase } from '@/lib/supabase'
 import { LinkIcon, EditIcon, TrashIcon, ChatIcon, CheckIcon } from '../../components/Icons'
 
